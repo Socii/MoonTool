650eb319da9f64e166b0f8cae0f88fed30761978
Socii Watts <sociiw@gmail.com>
2019-09-13 19:23:16 +0100

    Initial Commit

---
+++ MoonTool/MoonTool.h
@@ -0,0 +1,13 @@
+// MoonTool
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MoonTool.
+FOUNDATION_EXPORT double MoonToolVersionNumber;
+
+//! Project version string for MoonTool.
+FOUNDATION_EXPORT const unsigned char MoonToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoonTool/PublicHeader.h>
+
+
